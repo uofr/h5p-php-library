4291c982a0f6cf33674110d8effba0cc76b173a0
Joel Dapiawen <joel.dapiawen@uregina.ca>
2023-09-13 09:34:12 -0600

    Additional update for h5p resizer.js to fix reloading issue

--- js/h5p-resizer.js
+++ js/h5p-resizer.js
@@ -1,23 +1,25 @@
-//UOFR Hack dapiawej Custom H5P iframes resizer update September 11, 2023
+//UOFR Hack dapiawej Custom H5P iframes resizer update September 13, 2023
 //First we need to get iframes from H5P only.
 var iframes = document.querySelectorAll('iframe[src*="mod/hvp/embed.php"]');
 
 // Create an array to store the original height of each iframe
 var originalHeights = [];
 
-// Loop through each iframe and add the custom class
+// Loop through each iframe and add the custom class 
 for (var i = 0; i < iframes.length; i++) {
   var iframe = iframes[i];
   iframe.classList.add('h5p-custom-class');
-  //originalHeights.push(iframe.offsetHeight);
+
 }
 
-//var iframes = document.querySelectorAll('iframe');
-window.addEventListener('load', function() {
+
 for (var i = 0; i < iframes.length; i++) {
   const iframe = iframes[i];
 
-  //we are going to use Mutation observer since (message API eventlistener) is not reliable to resize the iframe and avoid flikering.
+// Add a load event listener that will wait for all the dom elements, 1st to load.
+window.addEventListener('load', function() {
+
+  //Use Mutation observer since (message API eventlistener) is not reliable to resize the iframe and avoid flikering.
   //which can lead to performance problems and unexpected behavior.
   const newObserver = new MutationObserver(function(mutationsList, observer) {
     for(var mutation of mutationsList) {
@@ -28,16 +30,15 @@ for (var i = 0; i < iframes.length; i++) {
       } else if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
         
         resizeIframes();
-        console.log('H5P iframe has been resized to the correct value');
+        
         break;
       }
     }
   });
 
   newObserver.observe(iframe.contentWindow.document, { childList: true, subtree: true, attributes: true, attributeFilter: ['style'] });
-
-}
 });
+}
 
 function resizeIframes() {
   // Loop through each iframe with the custom class
@@ -46,16 +47,7 @@ function resizeIframes() {
     var iframe = iframes[i];
     //console.log('Iframe ready:', iframe);
 
-   // Hide the completion button since we don't need it and avoid vertical scrollbars
-   /*const elements = iframe.contentWindow.document.querySelectorAll('.activity-header');
-   for (let j = 0; j < elements.length; j++) {
-     
-     elements[j].style.height = "0";
-     elements[j].style.width = "0";
-     elements[j].style.overflow = "hidden";
-     elements[j].style.visibility = "hidden";
-   }*/
-   
+  
     var sectionEmbed = iframe.contentWindow.document.querySelector('.embedded-main');
     var sectionHeight = sectionEmbed.offsetHeight;
     //console.log('Section height:', sectionHeight);
@@ -64,13 +56,12 @@ function resizeIframes() {
     iframe.style.height = newHeightWithExtra + 'px';
     iframe.style.width = '100%';
     console.log('Result - Iframe height + Section height:', newHeightWithExtra);
-
+    console.log('H5P iframe has been resized to the correct value');
   }
  
 }
-
 window.onload = function() {
-  
+  // Everything, including images and iframes, is fully loaded
   resizeIframes();
   console.log('Custom iframe resizing code loaded.');
 };
