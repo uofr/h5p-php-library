f9019cc1adf2f37bed90b17b9a0a6d41819563fe
John Lane <46030891+JohnOLane@users.noreply.github.com>
2025-07-31 11:13:40 -0600

    Add courseid and categoryid.

--- js/h5p.js
+++ js/h5p.js
@@ -379,10 +379,12 @@ H5P.init = function (target) {
     const contentLanguage = contentData && contentData.metadata && contentData.metadata.defaultLanguage
       ? contentData.metadata.defaultLanguage : 'en';
     const darkmodeclass = $iframe.data('darkmodeclass');
+    const courseclass = $iframe.data('courseid');
+    const categoryclass = $iframe.data('categoryid');
 
     const writeDocument = function () {
       iframe.contentDocument.open();
-      iframe.contentDocument.write('<!doctype html><html class="h5p-iframe ' + darkmodeclass + '" lang="' + contentLanguage + '"><head>' + H5P.getHeadTags(contentId) + '</head><body><div class="h5p-content" data-content-id="' + contentId + '"/></body></html>');
+      iframe.contentDocument.write('<!doctype html><html class="h5p-iframe ' + darkmodeclass + ' ' + courseclass + ' ' + categoryclass + ' ' + '" lang="' + contentLanguage + '"><head>' + H5P.getHeadTags(contentId) + '</head><body><div class="h5p-content" data-content-id="' + contentId + '"/></body></html>');
       iframe.contentDocument.close();
     };
 
